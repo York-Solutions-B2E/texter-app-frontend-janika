d42c30f5a11d5a6b54dac236ddfc3568fa353da3
jhortizu01 <jhortizuela21@gmail.com>
2025-01-17 11:24:00 -0700

    style messages

--- src/App.js
+++ src/App.js
@@ -5,38 +5,77 @@ import {useEffect, useState} from "react";
 
 function App() {
   const [messages, setMessages] = useState(null);
-
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     // Fetch data every second
-  //     fetch('http://localhost:8080/api/conversations/1')
-  //         .then((response) => response.json())
-  //         .then((data) => setMessages(data.messages))
-  //         .catch((error) => console.error('Error fetching data:', error));
-  //   }, 1000); // 1000ms = 1 second
-  //
-  //   // Cleanup interval on component unmount
-  //   return () => clearInterval(intervalId);
-  // }, []);
+  const [inputValue, setInputValue] = useState('');
+  const [currentUser, setCurrentUser] = useState({"id": 2, "username": "janika"})
+    // Handle the input change
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
   useEffect(() => {
-    // Fetch data once when the component mounts
-    fetch('http://localhost:8080/api/conversations/1')
-        .then((response) => response.json())
-        .then((data) => setMessages(data.messages))
-        .catch((error) => console.error('Error fetching data:', error));
+    const intervalId = setInterval(() => {
+      // Fetch data every second
+      fetch('http://localhost:8080/api/conversations/1')
+          .then((response) => response.json())
+          .then((data) => setMessages(data.messages))
+          .catch((error) => console.error('Error fetching data:', error));
+    }, 2000); // 1000ms = 1 second
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
+
 
   console.log(messages);
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Send a POST request
+        fetch('http://localhost:8080/api/messages/conversations/1', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "text": inputValue,
+                "status": "SENT",
+                "user": currentUser,
+                "timestamp": new Date().getTime(),
+            }), // send input value as payload
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                // Handle the response data
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+        setInputValue('');
+    };
+
+
+
   return (
       <div className="app">
-        <div className="messages-container">
-            { messages != null ? messages.map((message) => {
-                return <Messages key={message.id} message={message} />;
-            }) : <div>Send a message!</div>}
-        </div>
-        <input type="text" />
+          <div className="messages-container">
+              {messages != null ? messages
+                  .sort((a, b) => a.id - b.id)
+                  .map((message) => {
+                  return <Messages key={message.id} message={message} currentUser={currentUser} />;
+              }) : <div>Send a message!</div>}
+          </div>
+          <form onSubmit={handleSubmit}>
+              <input
+                  type="text"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                  placeholder="Enter something"
+              />
+              <button type="submit">Submit</button>
+          </form>
       </div>
   );
 }
--- src/messages/Messages.js
+++ src/messages/Messages.js
@@ -1,13 +1,103 @@
-import './styles.css'
+import React, {useEffect, useState} from 'react';
+import './styles.css';
+import editIcon from '../assets/edit.svg'
+import deleteIcon from '../assets/delete.svg'
+import saveIcon from '../assets/save.svg'
+import cancelIcon from '../assets/cancel.svg'
 
-function Messages({key, message}) {
-    console.log(message)
+function Messages({ key, message, currentUser}) {
+    const [messageText, setMessageText] = useState(message.text); // State to store message text
+    const [isEditing, setIsEditing] = useState(false); // State to track if the message is being edited
+
+    // Function to handle the update
+    const updateMessage = () => {
+        setIsEditing(true); // Enable editing
+    };
+
+    // Function to handle save after editing
+    const saveMessage = () => {
+        setIsEditing(false); // Disable editing
+        // Here, you can add any logic to save the updated message to the backend or wherever needed
+
+        fetch(`http://localhost:8080/api/messages/${message.id}/update`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                id: message.id,
+                text: messageText,
+                timestamp: new Date().toISOString().slice(0, 23),
+                status: "SENT",
+                user: currentUser
+            }), // send input value as payload
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                // Handle the response data
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    };
+
+    const deleteMessage = () => {
+        fetch(`http://localhost:8080/api/messages/${message.id}/delete`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+                // Handle the response data
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            })
+    }
+
+    const convertTimeStamp = (timestamp) => {
+        const date = new Date(timestamp);
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const seconds = date.getSeconds().toString().padStart(2, '0');
+
+        return `${hours}:${minutes}:${seconds}`;
+    }
     return (
-        <div key={key}>
-            <p><span>{message.user.username}</span> <span>{message.timestamp}</span>: {message.text} </p>
-            <span>{message.status}</span>
+        <div className="message" key={key}>
+            <div>
+                <p>
+                    <span className="username">{message.user.username}</span>
+                    <span className="timestamp">({convertTimeStamp(message.timestamp)})</span>:
+                    {message.status === "DELETE" ? (
+                        <span className="blurred">Message Deleted</span> // Apply blur effect
+                    ) : isEditing ? (
+                        <input
+                            type="text"
+                            value={messageText}
+                            onChange={(e) => setMessageText(e.target.value)} // Update message text as user types
+                        />
+                    ) : (
+                        <span className="message-text">{messageText}</span> // Display message text normally
+                    )}
+                </p>
+                {currentUser.id == message.user.id ?
+                    <div>
+                        <button onClick={isEditing ? saveMessage : updateMessage}>
+                            {isEditing ? <><img src={saveIcon} alt="save icon" /> <button><img src={cancelIcon}/></button></> :
+                                <img src={editIcon} alt="edit icon" />
+                            }
+                        </button>
+                        <button onClick={deleteMessage}><img src={deleteIcon} alt="delete" /></button> </div>: null}
+            </div>
+
+            <span className="message-status">{message.status === "DELETE" ? "Deleted" : message.status.charAt(0).toUpperCase() + message.status.slice(1).toLowerCase()}</span>
         </div>
-    )
+    );
 }
 
 export default Messages;
